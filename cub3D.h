431905585edc4e5041f84bfec43619301563180e
Alec LAVALLEE <alelaval@e2r7p5.42.fr>
2020-01-28 16:43:19 +0100

    Parsing en cours.

--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 11:38:23 by alelaval          #+#    #+#             */
-/*   Updated: 2020/01/28 10:31:48 by alelaval         ###   ########.fr       */
+/*   Updated: 2020/01/28 14:36:26 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,7 @@
 void	display_error(const char *error);
 void	parsing(char* file);
 void	save();
+void	parse_resolution();
+void	parse_sprite();
 
 #endif
--- parsing.c
+++ parsing.c
@@ -6,11 +6,31 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 11:37:57 by alelaval          #+#    #+#             */
-/*   Updated: 2020/01/28 10:34:42 by alelaval         ###   ########.fr       */
+/*   Updated: 2020/01/28 16:35:55 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+
+void	parse_textures(void)
+{
+	ft_putstr("Textures!\n");
+}
+
+void	parse_colors()
+{
+	ft_putstr("Colors!\n");
+}
+
+void	parse_resolution()
+{
+	ft_putstr("Resolution!\n");
+}
+void	parse_sprite()
+{
+	ft_putstr("Sprite!\n");
+}
 
 int		open_cub(char *file)
 {
@@ -37,16 +57,49 @@ int		open_cub(char *file)
 	return (fd);
 }
 
-char	**read_cub(char **map, int fd)
+char	**store_cub(char **map, char *file, int fd)
 {
 	char	*line;
+	size_t	i;
 
+	i = 0;
 	map = NULL;
+	line = NULL;
 	while ((get_next_line(fd, &line) == 1))
-	{
-		free(line);
-	}
+		i++;
+	close(fd);
+	map = (char**)malloc(sizeof(char*) * i + 1);
+	i = 0;
+	fd = open(file, O_RDONLY);
+	while ((get_next_line(fd, &line) == 1))
+		map[i++] = line;
+	map[i] = NULL;
 	return (map);
+}
+
+void	parse_cub(char **map)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (ft_isspace(map[i][j]))
+			j++;
+		if (map[i][j] == 'R')
+			parse_resolution();
+		else if (map[i][j] == 'S' && map[i][j] != 'O')
+			parse_sprite();
+		else if (ft_strncmp(&map[i][j], "NO", 2) == 0 || ft_strncmp(&map[i][j], "SO", 2) == 0 ||
+			ft_strncmp(&map[i][j], "WE", 2) == 0 || ft_strncmp(&map[i][j], "EA", 2) == 0)
+			parse_textures();
+		else if (map[i][j] == 'F' || map[i][j] == 'C')
+			parse_colors();
+		i++;
+	}
 }
 
 void	parsing(char *file)
@@ -57,7 +110,12 @@ void	parsing(char *file)
 	map = NULL;
 	fd = open_cub(file);
 	if (fd != -1)
-		map = read_cub(map, fd);
+	{
+		if ((map = store_cub(map, file, fd)))
+			parse_cub(map);
+		else
+			return ;
+	}
 	else
 		return ;
 	return ;
