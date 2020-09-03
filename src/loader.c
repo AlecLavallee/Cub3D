b638f315e06ab8d17598d8ec8abc3302f7582b9a
Alec LAVALLEE <alelaval@e2r12p10.42.fr>
2020-09-03 18:23:43 +0200

    Codage des fonctions de chargement des textures, a tester.

--- src/loader.c
+++ src/loader.c
@@ -6,18 +6,29 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 13:01:35 by alelaval          #+#    #+#             */
-/*   Updated: 2020/09/03 16:52:49 by alelaval         ###   ########.fr       */
+/*   Updated: 2020/09/03 17:30:39 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "cub3d.h"
+#include "cub3d.h"
 
-void	load_texture(t_cub *cub, void *path)
+void	load_tex(t_cub *cub, t_texture *tex_ptr)
 {
 	int		width;
 	int		height;
 
-	cub->map.textures.no.image.img_ptr = mlx_xpm_file_to_image(cub->mlx.mlx, \
-	"textures/bluestone.xpm", &cub->map.textures.no.image.width, &cub->map.textures.no);
+	width = 0;
+	height = 0;
+	tex_ptr = mlx_xpm_file_to_image(cub->mlx.mlx, \
+	"textures/bluestone.xpm", &width, &height);
+	tex_ptr->width = width;
+	tex_ptr->height = height;
+}
 
+void	load_textures(t_cub *cub)
+{
+	load_tex(cub, &cub->map.textures.no);
+	load_tex(cub, &cub->map.textures.so);
+	load_tex(cub, &cub->map.textures.we);
+	load_tex(cub, &cub->map.textures.ea);
 }
