c2bb5fef95d20360be27a7b1ec7192c670e555a6
Alec LAVALLEE <alelaval@e1r6p6.42.fr>
2020-09-16 15:21:31 +0200

    Set a NULL de toutes les variables de la structure cub et ajustement de la vitesse.

--- src/utils.c
+++ src/utils.c
@@ -3,20 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: macbook <macbook@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 11:37:52 by alelaval          #+#    #+#             */
-/*   Updated: 2020/09/07 16:25:13 by macbook          ###   ########.fr       */
+/*   Updated: 2020/09/16 15:06:14 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "libft.h"
 
+void	set_textures(t_cub *cub)
+{
+	cub->image.img_ptr = NULL;
+	cub->image.img_data = NULL;
+	cub->map.textures.no.image.img_ptr = NULL;
+	cub->map.textures.no.image.img_data = NULL;
+	cub->map.textures.so.image.img_ptr = NULL;
+	cub->map.textures.so.image.img_data = NULL;
+	cub->map.textures.we.image.img_ptr = NULL;
+	cub->map.textures.we.image.img_data = NULL;
+	cub->map.textures.ea.image.img_ptr = NULL;
+	cub->map.textures.ea.image.img_data = NULL;
+	cub->map.textures.sprite.image.img_ptr = NULL;
+	cub->map.textures.sprite.image.img_data = NULL;
+}
+
 t_cub	*init_cub(t_cub *cub)
 {
-	cub->mlx.screenWidth = -1;
-	cub->mlx.screenHeight = -1;
+	set_textures(cub);
+	cub->map.map = NULL;
+	cub->map.line = NULL;
+	cub->mlx.window = NULL;
 	cub->camera.posX = 8;
 	cub->camera.posY = 5;
 	cub->camera.dirX = -1;
@@ -24,7 +42,8 @@ t_cub	*init_cub(t_cub *cub)
 	cub->camera.planeX = 0.0;
 	cub->camera.planeY = 0.66;
 	cub->camera.hit = 0;
-	cub->camera.rotSpeed = 1.0;
+	cub->camera.rotSpeed = 0.15;
+	cub->player.moveSpeed = 0.15;
 	cub->file.mapping = 0;
 	return (cub);
 }
