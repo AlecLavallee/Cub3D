e82954acff0696935d7a482cbde285f556164f55
macbook <macbook@MacDeAlec.local>
2020-10-12 18:14:38 +0200

    Avancement sprite, probleme dedoublement.

--- src/sprites2.c
+++ src/sprites2.c
@@ -6,7 +6,7 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 15:17:39 by alelaval          #+#    #+#             */
-/*   Updated: 2020/10/09 18:22:49 by alelaval         ###   ########.fr       */
+/*   Updated: 2020/10/12 16:49:59 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,12 @@ unsigned	get_color_sprite(t_cub *cub)
 	unsigned	*color_ptr;
 	unsigned	color;
 
-	tex_ptr = get_tex_ptr(cub, 4);
+	tex_ptr = &cub->map.textures.sprite;
 	color_ptr = (unsigned*)mlx_get_data_addr(tex_ptr->image.img_ptr,
 	&tex_ptr->image.bpp, &tex_ptr->image.linesize, &tex_ptr->image.endian);
 	color = \
 	(unsigned)(color_ptr[TEX_WIDTH * cub->camera.texy + cub->camera.texx]);
-	if (cub->camera.side == 1)
-		color = (unsigned)(color >> 1) & 8355711;
 	return (color);
-}
-
-void		draw_sprite_row(t_cub *cub, int x, int y)
-{
-	unsigned	*img_data;
-	int			bpp;
-	int			size_line;
-	int			endian;
-	unsigned	color;
-
-	img_data = (unsigned*)mlx_get_data_addr(cub->image.img_ptr,
-	&bpp, &size_line, &endian);
-	color = get_color_sprite(cub);
-	if ((color & 0x00FFFFFF) != 0)
-		img_data[(y * size_line / 4) + x] = color;
 }
 
 void	sprite_transform(t_cub *cub)
@@ -60,7 +43,6 @@ void	sprite_calc(t_cub *cub)
 	cub->sprite.drawendy = cub->sprite.spriteheight / 2 + cub->mlx.screenheight / 2;
 	if (cub->sprite.drawendy >= cub->mlx.screenheight)
 		cub->sprite.drawendy = cub->mlx.screenheight - 1;
-
 	cub->sprite.spritewidth = abs((int)(cub->mlx.screenheight / cub->sprite.transformy));
 	cub->sprite.drawstartx = -cub->sprite.spritewidth / 2 + cub->sprite.spritescreenx;
 	if (cub->sprite.drawstartx < 0)
@@ -70,40 +52,62 @@ void	sprite_calc(t_cub *cub)
 		cub->sprite.drawendx = cub->mlx.screenwidth - 1;
 }
 
-void	sprite_draw(t_cub *cub, int stripe)
+void		draw_sprite_pixel(t_cub *cub, int x, int y, unsigned color)
 {
-	int y;
-	int d;
+	unsigned	*img_data;
+	int			bpp;
+	int			size_line;
+	int			endian;
 
-	cub->camera.texx = (int)(256 * (stripe - (-cub->sprite.spritewidth / 2 + cub->sprite.spritescreenx) * TEX_WIDTH / cub->sprite.spritewidth / 256));		printf("stripe : %d, transY : %f, zbuffer : %f\n", stripe, cub->sprite.transformy, cub->camera.zbuffer[stripe]);
-	if (cub->sprite.transformy > 0 && stripe > 0 && stripe < cub->mlx.screenwidth && cub->sprite.transformy < cub->camera.zbuffer[stripe])
+	img_data = (unsigned*)mlx_get_data_addr(cub->image.img_ptr, \
+	&bpp, &size_line, &endian);
+	img_data[(y * size_line / 4) + x] = (unsigned)color;
+}
+
+void	draw_textured_sprite(t_cub *cub, int stripe)
+{
+	int		y;
+	unsigned color;
+
+	y = cub->sprite.drawstarty;
+	while (y < cub->sprite.drawendy)
 	{
-		y = cub->sprite.drawstarty;
-		while (y < cub->sprite.drawendy)
-		{
-			d = (y) * 256 - cub->mlx.screenheight * 128 + cub->sprite.spriteheight * 128;
-			cub->camera.texy = ((d * TEX_HEIGHT) / cub->sprite.spriteheight) / 256;
-			draw_sprite_row(cub, stripe, y);
-			y++;
-		}
+		cub->camera.texy = (y) * 256 - cub->mlx.screenheight * 128 + cub->sprite.spriteheight * 128;
+		cub->camera.texy = ((cub->camera.texy * TEX_HEIGHT) / cub->sprite.spriteheight) / 256;
+		color = get_color_sprite(cub);
+		if ((color & 0x00FFFFFF) != 0)
+			draw_sprite_pixel(cub, stripe, y, color);
+		y++;
+	}
+}
+
+void	draw_sprite(t_cub *cub)
+{
+	int stripe;
+
+	cub->sprite.spritescreenx = (int)((cub->mlx.screenwidth / 2) * (1 + cub->sprite.transformx / cub->sprite.transformy));
+	sprite_calc(cub);
+	stripe = cub->sprite.drawstartx;
+	while (stripe < cub->sprite.drawendx)
+	{
+		cub->camera.texx = (int)(256 * (stripe - (-cub->sprite.spritewidth / 2 + cub->sprite.spritescreenx) * TEX_WIDTH / cub->sprite.spritewidth) / 256);
+		if (cub->sprite.transformy > 0 && stripe > 0 && stripe < cub->mlx.screenwidth && cub->sprite.transformy < cub->camera.zbuffer[stripe])
+			draw_textured_sprite(cub, stripe);
+		stripe++;
 	}
 }
 
 void	draw_sprites(t_cub *cub, t_s *sprites)
 {
 	int i;
-	int stripe;
 
-	i = -1;
-	while (++i < cub->sprite.numsprites)
+	i = 0;
+	while (i < cub->sprite.numsprites)
 	{
 		cub->sprite.spritex = sprites[i].pos.x - cub->camera.posx;
 		cub->sprite.spritey = sprites[i].pos.y - cub->camera.posy;
 		sprite_transform(cub);
-		cub->sprite.spritescreenx = (int)((cub->mlx.screenwidth / 2) * (1 + cub->sprite.transformx / cub->sprite.transformy));
-		sprite_calc(cub);
-		stripe = cub->sprite.drawstartx;
-		while (stripe < cub->sprite.drawendx)
-			sprite_draw(cub, stripe++);
+		draw_sprite(cub);
+		i++;
 	}
 }
