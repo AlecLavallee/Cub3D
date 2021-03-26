81f23e56244c715d2d4b96f0992733799162c41d
alelaval <alec.lavallee89@gmail.com>
2021-03-26 14:54:40 +0100

    Correction fichier save.

--- headers/save.h
+++ headers/save.h
@@ -19,35 +19,9 @@
 # define FILE_SIZE	14
 # define INFO_SIZE	40
 
-typedef struct	s_bmp_file_header {
-	uint16_t	file_type;
-	uint32_t	file_size;
-	uint16_t	reserved1;
-	uint16_t	reserved2;
-	uint32_t	offset;
-}				t_bmp_file_header;
-
-typedef struct	s_bmp_info_header {
-	uint32_t	header_size;
-	int32_t		img_width;
-	int32_t		img_height;
-	uint16_t	planes;
-	uint16_t	bit_per_pix;
-	uint32_t	compression;
-	uint32_t	img_size;
-	int32_t		x_pixels_per_meter;
-	int32_t		y_pixels_per_meter;
-	uint32_t	colors_used;
-	uint32_t	colors_important;
-}				t_bmp_info_header;
-
-typedef struct	s_bmp {
-	t_bmp_file_header	file_header;
-	t_bmp_info_header	info_header;
-	uint8_t				*data;
-}				t_bmp;
-
+void	save_error(void);
 void	save(t_cub *cub, const char *file);
-t_bmp   write_metadata(void *img_mlx, unsigned *res);
+void	write_metadata(t_cub *cub, int fd, t_bmp file);
+void	write_data(t_cub *cub, int fd, t_bmp file);
 
 #endif
